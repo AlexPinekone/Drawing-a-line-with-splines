c21bee092b393f75dafaf613c9d4b3fa63c9628e
AlexPinekone <127763929+AlexPinekone@users.noreply.github.com>
2024-09-11 19:12:48 -0600

    Add files via upload

---
+++ Grafica/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Grafica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelGrafica = new System.Windows.Forms.Panel();
+            this.x1Linea = new System.Windows.Forms.TextBox();
+            this.bLinea = new System.Windows.Forms.Button();
+            this.y1Linea = new System.Windows.Forms.TextBox();
+            this.x2Linea = new System.Windows.Forms.TextBox();
+            this.y2Linea = new System.Windows.Forms.TextBox();
+            this.yfCurva = new System.Windows.Forms.TextBox();
+            this.xfCurva = new System.Windows.Forms.TextBox();
+            this.y2Curva = new System.Windows.Forms.TextBox();
+            this.bCurva = new System.Windows.Forms.Button();
+            this.x2Curva = new System.Windows.Forms.TextBox();
+            this.y1Curva = new System.Windows.Forms.TextBox();
+            this.x1Curva = new System.Windows.Forms.TextBox();
+            this.yiCurva = new System.Windows.Forms.TextBox();
+            this.xiCurva = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panelGrafica
+            // 
+            this.panelGrafica.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelGrafica.Location = new System.Drawing.Point(12, 13);
+            this.panelGrafica.Name = "panelGrafica";
+            this.panelGrafica.Size = new System.Drawing.Size(456, 425);
+            this.panelGrafica.TabIndex = 0;
+            this.panelGrafica.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGrafica_Paint);
+            this.panelGrafica.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelGrafica_MouseDown);
+            this.panelGrafica.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelGrafica_MouseMove);
+            this.panelGrafica.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelGrafica_MouseUp);
+            // 
+            // x1Linea
+            // 
+            this.x1Linea.Location = new System.Drawing.Point(514, 15);
+            this.x1Linea.Name = "x1Linea";
+            this.x1Linea.Size = new System.Drawing.Size(82, 26);
+            this.x1Linea.TabIndex = 1;
+            // 
+            // bLinea
+            // 
+            this.bLinea.Location = new System.Drawing.Point(585, 112);
+            this.bLinea.Name = "bLinea";
+            this.bLinea.Size = new System.Drawing.Size(100, 37);
+            this.bLinea.TabIndex = 5;
+            this.bLinea.Text = "Linea";
+            this.bLinea.UseVisualStyleBackColor = true;
+            this.bLinea.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // y1Linea
+            // 
+            this.y1Linea.Location = new System.Drawing.Point(676, 15);
+            this.y1Linea.Name = "y1Linea";
+            this.y1Linea.Size = new System.Drawing.Size(82, 26);
+            this.y1Linea.TabIndex = 2;
+            // 
+            // x2Linea
+            // 
+            this.x2Linea.Location = new System.Drawing.Point(514, 61);
+            this.x2Linea.Name = "x2Linea";
+            this.x2Linea.Size = new System.Drawing.Size(82, 26);
+            this.x2Linea.TabIndex = 3;
+            // 
+            // y2Linea
+            // 
+            this.y2Linea.Location = new System.Drawing.Point(676, 61);
+            this.y2Linea.Name = "y2Linea";
+            this.y2Linea.Size = new System.Drawing.Size(82, 26);
+            this.y2Linea.TabIndex = 4;
+            // 
+            // yfCurva
+            // 
+            this.yfCurva.Location = new System.Drawing.Point(676, 336);
+            this.yfCurva.Name = "yfCurva";
+            this.yfCurva.Size = new System.Drawing.Size(82, 26);
+            this.yfCurva.TabIndex = 13;
+            // 
+            // xfCurva
+            // 
+            this.xfCurva.Location = new System.Drawing.Point(514, 338);
+            this.xfCurva.Name = "xfCurva";
+            this.xfCurva.Size = new System.Drawing.Size(82, 26);
+            this.xfCurva.TabIndex = 12;
+            // 
+            // y2Curva
+            // 
+            this.y2Curva.Location = new System.Drawing.Point(676, 287);
+            this.y2Curva.Name = "y2Curva";
+            this.y2Curva.Size = new System.Drawing.Size(82, 26);
+            this.y2Curva.TabIndex = 11;
+            // 
+            // bCurva
+            // 
+            this.bCurva.Location = new System.Drawing.Point(585, 386);
+            this.bCurva.Name = "bCurva";
+            this.bCurva.Size = new System.Drawing.Size(100, 37);
+            this.bCurva.TabIndex = 14;
+            this.bCurva.Text = "Curva";
+            this.bCurva.UseVisualStyleBackColor = true;
+            this.bCurva.Click += new System.EventHandler(this.bCurva_Click);
+            // 
+            // x2Curva
+            // 
+            this.x2Curva.Location = new System.Drawing.Point(514, 293);
+            this.x2Curva.Name = "x2Curva";
+            this.x2Curva.Size = new System.Drawing.Size(82, 26);
+            this.x2Curva.TabIndex = 10;
+            // 
+            // y1Curva
+            // 
+            this.y1Curva.Location = new System.Drawing.Point(676, 239);
+            this.y1Curva.Name = "y1Curva";
+            this.y1Curva.Size = new System.Drawing.Size(82, 26);
+            this.y1Curva.TabIndex = 9;
+            // 
+            // x1Curva
+            // 
+            this.x1Curva.Location = new System.Drawing.Point(514, 244);
+            this.x1Curva.Name = "x1Curva";
+            this.x1Curva.Size = new System.Drawing.Size(82, 26);
+            this.x1Curva.TabIndex = 8;
+            // 
+            // yiCurva
+            // 
+            this.yiCurva.Location = new System.Drawing.Point(676, 190);
+            this.yiCurva.Name = "yiCurva";
+            this.yiCurva.Size = new System.Drawing.Size(82, 26);
+            this.yiCurva.TabIndex = 7;
+            // 
+            // xiCurva
+            // 
+            this.xiCurva.Location = new System.Drawing.Point(514, 196);
+            this.xiCurva.Name = "xiCurva";
+            this.xiCurva.Size = new System.Drawing.Size(82, 26);
+            this.xiCurva.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(483, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 20);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "x1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(645, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "y1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(483, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "x2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(645, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 20);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "y2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(483, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "x1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(483, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 20);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "x2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(483, 296);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 20);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "x3";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(483, 344);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 20);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "x4";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(645, 338);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(25, 20);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "y4";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(645, 290);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(25, 20);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "y3";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(645, 241);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(25, 20);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "y2";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(645, 193);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(25, 20);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "y1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.y1Curva);
+            this.Controls.Add(this.x1Curva);
+            this.Controls.Add(this.yiCurva);
+            this.Controls.Add(this.xiCurva);
+            this.Controls.Add(this.yfCurva);
+            this.Controls.Add(this.xfCurva);
+            this.Controls.Add(this.y2Curva);
+            this.Controls.Add(this.bCurva);
+            this.Controls.Add(this.x2Curva);
+            this.Controls.Add(this.y2Linea);
+            this.Controls.Add(this.x2Linea);
+            this.Controls.Add(this.y1Linea);
+            this.Controls.Add(this.bLinea);
+            this.Controls.Add(this.x1Linea);
+            this.Controls.Add(this.panelGrafica);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelGrafica;
+        private System.Windows.Forms.TextBox x1Linea;
+        private System.Windows.Forms.Button bLinea;
+        private System.Windows.Forms.TextBox y1Linea;
+        private System.Windows.Forms.TextBox x2Linea;
+        private System.Windows.Forms.TextBox y2Linea;
+        private System.Windows.Forms.TextBox yfCurva;
+        private System.Windows.Forms.TextBox xfCurva;
+        private System.Windows.Forms.TextBox y2Curva;
+        private System.Windows.Forms.Button bCurva;
+        private System.Windows.Forms.TextBox x2Curva;
+        private System.Windows.Forms.TextBox y1Curva;
+        private System.Windows.Forms.TextBox x1Curva;
+        private System.Windows.Forms.TextBox yiCurva;
+        private System.Windows.Forms.TextBox xiCurva;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Grafica/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Grafica
+{
+    public partial class Form1 : Form
+    {
+        //Variables necesarias
+        //Lo que dicen lol
+        private bool estaArrastrando1 = false;
+        private bool estaArrastrando2 = false;
+        private bool estaArrastrando3 = false;
+        private bool estaArrastrando4 = false;
+        //Los dos objetos para mover
+        private Rectangle movi1;
+        private Rectangle movi2;
+        private Rectangle moviI;
+        private Rectangle moviF;
+        //La posicion de esos dos objetos 
+        private Point posicionI1, posicionI2, posicionI3, posicionI4;
+        //Saber si esta una curva activa
+        private bool hayCurva;
+        //Los valores de la curva deben ser globales para cargarla cada vez que se actualice todo
+        private int xI, yI, xF, yF, x1, y1, x2, y2;
+
+
+        //Se crea la ventana y se limpia el Panel
+        public Form1()
+        {
+            InitializeComponent();
+            LimpiarPanel();
+        }
+        //Se limpia el panel y se crean dos lineas 
+        private void LimpiarPanel()
+        {
+            panelGrafica.Invalidate();
+            panelGrafica.Update();
+            //Las lineas del mapa cartesiano
+            generaLinea(150, 0, 150, 275, false);
+            generaLinea(0, 137, 300, 137, false);
+
+
+        }
+        
+        //Al presionar el boton de linea
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //limpia la ventana
+
+            movi1.X = movi1.Y = movi2.X = movi2.Y = 1000;
+            
+            LimpiarPanel();
+            try
+            {
+                //Define las cuatro variables de los dos puntos
+                int xInicial, yInicial, xFinal, yFinal;
+                //Los convierte de texto a enteros
+                xInicial = int.Parse(x1Linea.Text);
+                yInicial = int.Parse(y1Linea.Text);
+                xFinal = int.Parse(x2Linea.Text);
+                yFinal = int.Parse(y2Linea.Text);
+                //Llama al metodo que lo dibuja
+                generaLinea(xInicial, yInicial, xFinal, yFinal, true);
+            }catch(Exception eme)
+            {
+                MessageBox.Show("valores no validos");
+            }
+        }
+        //Eso.. la genera... la linea
+        private void generaLinea(int xl1,int yl1, int xl2, int yl2,bool col)
+        {
+            //Acomoda los puntos al centro del plano cartesiano(las "y" se restan porque
+            //normalmente van a la inversa al graficar)
+            //El IF es para saber si esta dibujando una linea, o el plano mismo
+            if (col == true)
+            {
+                xl1 = xl1 + 150;
+                yl1 = 137 - yl1;
+                xl2 = xl2 + 150;
+                yl2 = 137 - yl2;
+            }
+            //Cosa para dibujar
+            Graphics g = panelGrafica.CreateGraphics();
+            //DDA
+            int dx= xl2 - xl1;
+            int dy= yl2 - yl1;
+            //Toma el mas grande
+            int pasos = Math.Max(Math.Abs(dx), Math.Abs(dy)); 
+            //Calcula los incrementos
+            float incrementX = (float)dx / pasos; 
+            float incrementY = (float)dy / pasos; 
+            //A lo que se le va a ir sumando sin afectar los originales
+            float x = xl1;
+            float y = yl1;
+            //Ciclo que hace cosas de ciclo
+            for (int i = 0; i <= pasos; i++)
+            {
+                //Con el if cambia el color del plano cartesiano, y la linea
+                if(col==true)
+                    g.FillRectangle(Brushes.Purple, x, y, 1, 1); 
+                else
+                    g.FillRectangle(Brushes.Black, x, y, 1, 1);
+                x += incrementX;
+                y += incrementY;
+            }
+        
+        }
+
+        //Boton de la curva
+        private void bCurva_Click(object sender, EventArgs e)
+        {
+            //Limpia el panel
+            LimpiarPanel();
+            //Asigna todos los valores globales
+            try { 
+            xI = int.Parse(xiCurva.Text);
+            yI = int.Parse(yiCurva.Text);
+            x1 = int.Parse(x1Curva.Text);
+            y1 = int.Parse(y1Curva.Text);
+            x2 = int.Parse(x2Curva.Text);
+            y2 = int.Parse(y2Curva.Text);
+            xF = int.Parse(xfCurva.Text);
+            yF = int.Parse(yfCurva.Text);
+            //Asigna donde deben estar los dos objetos que se mueven
+            movi1 = new Rectangle(x1+150, 137-y1, 10, 10);
+            movi2 = new Rectangle(x2+150, 137-y2, 10, 10);
+                moviI = new Rectangle(xI + 150, 137 - yI, 10, 10);
+                moviF = new Rectangle(xF + 150, 137 - yF, 10, 10);
+                //Llama al metodo que genera la curva
+                generaCurva(xI,yI,x1,y1,x2,y2,xF,yF);
+            }catch(Exception eme)
+            {
+                MessageBox.Show("valores no validos");
+            }
+
+}
+        //Pasa los valores así(de uno por uno) para no modificar los valores globales por error
+        private void generaCurva(int xcI,int ycI,int xc1,int yc1,int xc2,int yc2,int xcF,int ycF)
+        {
+            //Actualiza el panel
+            panelGrafica.Paint += panelGrafica_Paint;
+            LimpiarPanel();
+            //Acomoda todo al plano cartesiano
+            xcI = xcI + 150;
+            ycI = 137 - ycI;
+            xc1 = xc1 + 150;
+            yc1 = 137 - yc1;
+            xc2 = xc2 + 150;
+            yc2 = 137 - yc2;
+            xcF = xcF + 150;
+            ycF = 137 - ycF;
+            //Varibles para generar la curva como el tiempo, y que tanto aumenta
+            double t;
+            double incremento = 0.001;
+            double fx, fy;
+            Graphics g = panelGrafica.CreateGraphics();
+            //Cada tiempo dibuja un punto siguiendo las formaulas de Bezier
+            for (t = 0; t < 1; t += incremento)
+            {
+                //Para la x
+                fx = (Math.Pow(1 - t, 3) * xcI) + 
+                    (3*t*(Math.Pow(1-t,2))*xc1) + 
+                    (3*(Math.Pow(t,2))*(1-t)*xc2) + 
+                    (Math.Pow(t,3)*xcF);
+                //Para la y
+                fy = (Math.Pow(1 - t, 3) * ycI) +
+                    (3 * t * (Math.Pow(1 - t, 2)) * yc1) +
+                    (3 * (Math.Pow(t, 2)) * (1 - t) * yc2) +
+                    (Math.Pow(t, 3) * ycF);
+                //También se podría agregar una para z si es necesario
+
+                //Dibuja un rectangulito que compone la linea
+                g.FillRectangle(Brushes.Purple,(float)fx, (float)fy, 1, 1);
+            }
+
+        }
+
+        
+        //Vuelve a pintar los rectangulos moviles
+        private void panelGrafica_Paint(object sender, PaintEventArgs e)
+        {
+
+                e.Graphics.FillRectangle(Brushes.Blue, movi1);
+                e.Graphics.FillRectangle(Brushes.Blue, movi2);
+            e.Graphics.FillRectangle(Brushes.Blue, moviI);
+            e.Graphics.FillRectangle(Brushes.Blue, moviF);
+
+
+
+        }
+        //Revisa si estas presionando sobre uno de los dos rectangulos y activa la variable
+        // de que lo estan arrastrando al pobre
+        private void panelGrafica_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (movi1.Contains(e.Location))
+            {
+                estaArrastrando1 = true;
+                posicionI1 = e.Location;
+            }
+            if (movi2.Contains(e.Location))
+            {
+                estaArrastrando2 = true;
+                posicionI2 = e.Location;
+            }
+            if(moviI.Contains(e.Location))
+            {
+                estaArrastrando3 = true;
+                posicionI3 = e.Location;
+            }
+            if (moviF.Contains(e.Location))
+            {
+                estaArrastrando4 = true;
+                posicionI4 = e.Location;
+            }
+        }
+        //Cuando lo suelte
+        private void panelGrafica_MouseUp(object sender, MouseEventArgs e)
+        {
+            estaArrastrando1 = false;
+            estaArrastrando2 = false;
+            estaArrastrando3 = false;
+            estaArrastrando4 = false;
+        }
+        //Checa si esta arrastrando uno u otro
+        private void panelGrafica_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (estaArrastrando1)
+            {
+                // Calcula a donde se movio y se lo suma a la posicion del rectangulo
+                int deltaX = e.X - posicionI1.X;
+                int deltaY = e.Y - posicionI1.Y;
+                movi1.X += deltaX;
+                movi1.Y += deltaY;
+
+                //AAAAAAAAAAAA
+                // Cambia la posicion, puede que aqui haya un error por sumar un numero muy grande o
+                //algo asi
+                posicionI1 = e.Location;
+
+                //Cambia los valores en los textBox
+                x1Curva.Text = "" + (movi1.X-150);
+                y1Curva.Text = "" + (137-movi1.Y);
+                x1 = movi1.X-150;
+                y1 = 137-movi1.Y;
+                // Vuelve a dibujar la linea (dibuja tambien las otras cosas)
+                generaCurva(xI, yI, movi1.X-150,137- movi1.Y, x2, y2, xF, yF);
+               
+            }
+
+            if (estaArrastrando2)
+            {
+                // Calcula a donde se movio y se lo suma a la posicion del rectangulo
+                int deltaX = e.X - posicionI2.X;
+                int deltaY = e.Y - posicionI2.Y;
+                movi2.X += deltaX;
+                movi2.Y += deltaY;
+
+                //POR AQUI!!?
+                // Cambia la posicion, puede que aqui haya un error por sumar un numero muy grande o
+                //algo asi
+                posicionI2 = e.Location;
+                //Cambia los valores en los textBox
+                x2Curva.Text = ""+(movi2.X-150);
+                y2Curva.Text = "" + (137-movi2.Y);
+                x2 = movi2.X - 150;
+                y2 = 137 - movi2.Y;
+                // Vuelve a dibujar la linea (dibuja tambien las otras cosas)
+                generaCurva(xI, yI, x1, y1, movi2.X-150, 137-movi2.Y, xF, yF);
+
+            }
+
+            if (estaArrastrando3)
+            {
+                // Calcula a donde se movio y se lo suma a la posicion del rectangulo
+                int deltaX = e.X - posicionI3.X;
+                int deltaY = e.Y - posicionI3.Y;
+                moviI.X += deltaX;
+                moviI.Y += deltaY;
+
+                //POR AQUI!!?
+                // Cambia la posicion, puede que aqui haya un error por sumar un numero muy grande o
+                //algo asi
+                posicionI3 = e.Location;
+                //Cambia los valores en los textBox
+                xiCurva.Text = "" + (moviI.X - 150);
+                yiCurva.Text = "" + (137 - moviI.Y);
+                xI = moviI.X - 150;
+                yI = 137 - moviI.Y;
+                // Vuelve a dibujar la linea (dibuja tambien las otras cosas)
+                generaCurva(xI, yI, x1, y1, x2, y2, xF, yF);
+
+            }
+            if (estaArrastrando4)
+            {
+                // Calcula a donde se movio y se lo suma a la posicion del rectangulo
+                int deltaX = e.X - posicionI4.X;
+                int deltaY = e.Y - posicionI4.Y;
+                moviF.X += deltaX;
+                moviF.Y += deltaY;
+
+                //POR AQUI!!?
+                // Cambia la posicion, puede que aqui haya un error por sumar un numero muy grande o
+                //algo asi
+                posicionI4 = e.Location;
+                //Cambia los valores en los textBox
+                xfCurva.Text = "" + (moviF.X - 150);
+                yfCurva.Text = "" + (137 - moviF.Y);
+                xF = moviF.X - 150;
+                yF = 137 - moviF.Y;
+                // Vuelve a dibujar la linea (dibuja tambien las otras cosas)
+                generaCurva(xI, yI, x1, y1, x2, y2, xF, yF);
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
